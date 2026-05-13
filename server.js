2e7a53a0d6afe356582e3afe369ee0aaed1e1fcf
pedro <pedro.costa.122@ufrn.edu.br>
2026-05-13 18:45:28 -0300

    url certa

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import app from "./app.js";
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}/api/books`)
+    console.log(`Servidor rodando em http://localhost:${PORT}/api/books/bookshelf`)
 });
